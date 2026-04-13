c536e1be725d9cc8ac351f231023ef13adc04374
fzazammm <fawwazazam09670@gmail.com>
2026-04-13 20:32:24 +0700

    feat(models): add Enums.hpp

--- include/models/Enums.hpp
+++ include/models/Enums.hpp
@@ -1,1 +1,42 @@
 #pragma once
+
+enum class TileCategory {
+    PROPERTY,
+    ACTION,
+    SPECIAL
+};
+
+enum class ColorGroup {
+    COKLAT,
+    BIRU_MUDA,
+    MERAH_MUDA,
+    ORANGE,
+    MERAH,
+    KUNING,
+    HIJAU,
+    BIRU_TUA,
+    ABU_ABU,
+    DEFAULT
+};
+
+enum class PlayerStatus {
+    ACTIVE,
+    JAILED,
+    BANKRUPT
+};
+
+enum class PropertyStatus {
+    BANK,
+    OWNED,
+    MORTGAGED
+};
+
+enum class TaxType {
+    PPH,
+    PBM
+};
+
+enum class CardType {
+    CHANCE,
+    COMMUNITY_CHEST
+};
