bf4fbe39f9d84ea65117eb13a3ea4e10ffe5204a
ucupsss <yusuffaishalals@gmail.com>
2026-04-14 10:53:36 +0700

    feat : add config and state .hpp

--- include/models/state/PlayerState.hpp
+++ include/models/state/PlayerState.hpp
@@ -1,1 +1,37 @@
 #pragma once
+
+#include <string>
+#include <vector>
+
+#include "models/Enums.hpp"
+
+class PlayerState {
+private:
+    std::string username;
+    int balance;
+    int position;
+    PlayerStatus status;
+    int jailTurns;
+    std::vector<std::string> cardHand;
+
+public:
+    PlayerState();
+    PlayerState(
+        const std::string& username,
+        int balance,
+        int position,
+        PlayerStatus status,
+        int jailTurns,
+        const std::vector<std::string>& cardHand
+    );
+
+    const std::string& getUsername() const;
+    int getBalance() const;
+    int getPosition() const;
+    PlayerStatus getStatus() const;
+    int getJailTurns() const;
+    std::vector<std::string>& getCardHand();
+    const std::vector<std::string>& getCardHand() const;
+};
+
+
