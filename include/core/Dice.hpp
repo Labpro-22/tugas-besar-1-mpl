c9cf76ad5c12586329e328899243f5b3761819db
NathanaelGun <nathanifolia@gmail.com>
2026-04-14 23:51:28 +0700

    feat: add coreheader

--- include/core/Dice.hpp
+++ include/core/Dice.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+class Dice{
+    private:
+        int die1;
+        int die2;
+
+    public:
+        Dice();
+        void roll();
+        void setManual(int d1, int d2);
+        int getDie1() const;
+        int getDie2() const;
+        int getTotal() const;
+        bool isDouble() const;
+};
