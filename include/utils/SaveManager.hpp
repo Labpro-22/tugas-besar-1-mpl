9f831c1dc9d152e2e18a56932f29bc856635ee26
NathanaelGun <nathanifolia@gmail.com>
2026-04-15 11:40:50 +0700

    feat: add utils hpp including exceptions hpp

--- include/utils/SaveManager.hpp
+++ include/utils/SaveManager.hpp
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <string>
+
+class GameState;
+class PlayerState;
+class PropertyState;
+class LogEntry;
+
+
+class SaveManager {
+private:
+    std::string filePath;
+    std::string serializePlayer(const PlayerState& ps) const;
+    std::string serializeProperty(const PropertyState& ps) const;
+    std::string serializeDeck(const std::vector<std::string>& deck) const;
+    std::string serializeLog(const std::vector<LogEntry>& entries) const;
+
+    PlayerState parsePlayer(const std::string& line) const; 
+    PropertyState parseProperty(const std::string& line) const;
+
+public:
+    SaveManager() = default;
+    void saveGame(const std::string& filename, const GameState& gameState) const;
+    GameState loadGame(const std::string& filename) const;
+    bool fileExists(const std::string& filename) const;
+};
--- include/utils/exceptions/InvalidCommandException.hpp
+++ include/utils/exceptions/InvalidCommandException.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "NimonspoliException.hpp"
+#include <string>
+ 
+class InvalidCommandException : public NimonspoliException {
+private:
+    std::string commandKeyword;
+    std::string reason;
+ 
+public:
+    InvalidCommandException(const std::string& keyword, const std::string& reason)
+        : NimonspoliException("Perintah '" + keyword + "': " + reason),
+            commandKeyword(keyword), reason(reason) {}
+ 
+    const std::string& getCommandKeyword() const {return commandKeyword;}
+    const std::string& getReason() const {return reason;}
+};
